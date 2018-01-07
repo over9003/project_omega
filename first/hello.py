c0721ba6b6c3f56ccf53ff5a3a9e312f16287962
gabriel <gabrielvartolomeu@yahoo.com>
2018-01-07 20:57:32 +0200

    [first] first commit

--- first/hello.py
+++ first/hello.py
@@ -3,6 +3,16 @@ class Test(object):
 	@staticmethod
 	def FirstMethod():
 		print "this is the first method"
+	
+	@staticmethod
+	def SecondMethod():
+		print "this is the second method"
 
+	@staticmethod
+	def ThirdMethod():
+		print "this is the thidr method"
+	
 if '__init__' == '__main__':
 	Test.FirstMethod()
+	Test.SecondMethod()
+	Test.ThirdMethod()
